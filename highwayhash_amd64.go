9cb37b8cb4e3ed1cf7e0fda944dce3e64c35bddc
Klaus Post <klauspost@gmail.com>
2023-01-03 17:34:20 +0100

    Fix build tags for newer Go.

--- highwayhash_amd64.go
+++ highwayhash_amd64.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a license that can be
 // found in the LICENSE file.
 
+//go:build amd64 && !gccgo && !appengine && !nacl && !noasm
 // +build amd64,!gccgo,!appengine,!nacl,!noasm
 
 package highwayhash
