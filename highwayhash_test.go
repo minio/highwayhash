2bf548c66d740593faf9cefc5c50f4d32815e5b7
Frank Wessels <fwessels@xs4all.nl>
2024-06-25 06:31:03 +0000

    Add separate test case for Update function

--- highwayhash_test.go
+++ highwayhash_test.go
@@ -8,12 +8,66 @@ import (
 	"bytes"
 	"encoding/binary"
 	"encoding/hex"
+	"fmt"
 	"hash"
 	"math/rand"
 	"runtime"
+	"strings"
 	"sync/atomic"
 	"testing"
 )
+
+func TestUpdateFunc(t *testing.T) {
+
+	const want = `0xea006216335db212 - 0x97280efedaccd722
+0x2a0ba13d83848de6 - 0x476fe817e850c8cd
+0x5b8176dd6d555cf6 - 0x5740be93095fd2b5
+0xda9d392c1fd04836 - 0x28398f8f4d922993
+0x41a1794ac4aec11e - 0x41123afc4b6d0e35
+0x761f6fbe30a051ce - 0x6a4afc2b722e4155
+0x3f0887c550d61850 - 0x3368c4779ce6c2c8
+0xd1f892c21e69b968 - 0x974e980803544d90`
+
+	var state [16]uint64
+	state[0] = 0x0102030405060708
+	state[1] = 0x090a0b0c0d0e0f10
+	state[2] = 0x1112131415161718
+	state[3] = 0x191a1b1c1d1e1f20
+	state[4] = 0x2122232425262728
+	state[5] = 0x292a2b2c2d2e2f30
+	state[6] = 0x3132333435363738
+	state[7] = 0x393a3b3c3d3e3f40
+	state[8] = 0x4142434445464748
+	state[9] = 0x494a4b4c4d4e4f50
+	state[10] = 0x5152535455565758
+	state[11] = 0x595a5b5c5d5e5f60
+	state[12] = 0x6162636465666768
+	state[13] = 0x696a6b6c6d6e6f70
+	state[14] = 0x7172737475767778
+	state[15] = 0x797a7b7c7d7e7f80
+
+	msg := make([]byte, 64)
+	for i := range msg {
+		msg[i] = 0x33 + byte(i)
+	}
+
+	updateArm64Sve(&state, msg)
+
+	buf := bytes.Buffer{}
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[0], state[1]))
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[2], state[3]))
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[4], state[5]))
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[6], state[7]))
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[8], state[9]))
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[10], state[11]))
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[12], state[13]))
+	buf.WriteString(fmt.Sprintf("0x%016x - 0x%016x\n", state[14], state[15]))
+
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Errorf("TestUpdateFunc:\ngot: \n%v\nwant: \n%v", got, want)
+	}
+}
 
 func TestVectors(t *testing.T) {
 	defer func(sse4, avx2, neon, vmx bool) {
